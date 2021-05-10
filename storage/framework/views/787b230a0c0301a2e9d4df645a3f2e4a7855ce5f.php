cbf5cb84a70395142dd03d655fc7cf1b161345a7
khawarwaraich1995 <57666092+khawarwaraich1995@users.noreply.github.com>
2021-05-10 10:50:39 -0700

    fontfix

---
+++ storage/framework/views/787b230a0c0301a2e9d4df645a3f2e4a7855ce5f.php
@@ -0,0 +1,104 @@
+<?php $__env->startSection('admin_title'); ?>
+    <?php echo e(__('Restaurants')); ?>
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('restorants.partials.modals', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+    </div>
+
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0"><?php echo e(__('Restaurants')); ?></h3>
+                            </div>
+                            <div class="col-4 text-right">
+                                <a href="<?php echo e(route('admin.restaurants.create')); ?>" class="btn btn-sm btn-primary"><?php echo e(__('Add Restaurant')); ?></a>
+                                <?php if(auth()->user()->hasRole('admin') && config('settings.enable_import_csv')): ?>
+                                    <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modal-import-restaurants"><?php echo e(__('Import from CSV')); ?></button>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <?php echo $__env->make('partials.flash', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col"><?php echo e(__('Name')); ?></th>
+                                    <th scope="col"><?php echo e(__('Logo')); ?></th>
+                                    <th scope="col"><?php echo e(__('Owner')); ?></th>
+                                    <th scope="col"><?php echo e(__('Owner email')); ?></th>
+                                    <th scope="col"><?php echo e(__('Creation Date')); ?></th>
+                                    <th scope="col"><?php echo e(__('Active')); ?></th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $__currentLoopData = $restorants; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $restorant): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <tr>
+                                        <td><a href="<?php echo e(route('admin.restaurants.edit', $restorant)); ?>"><?php echo e($restorant->name); ?></a></td>
+                                        <td><img class="rounded" src=<?php echo e($restorant->icon); ?> width="50px" height="50px"></img></td>
+                                        <td><?php echo e($restorant->user?$restorant->user->name:__('Deleted')); ?></td>
+                                        <td>
+                                            <a href="mailto: <?php echo e($restorant->user?$restorant->user->email:""); ?>"><?php echo e($restorant->user?$restorant->user->email:__('Deleted')); ?></a>
+                                        </td>
+                                        <td><?php echo e($restorant->created_at->format(config('settings.datetime_display_format'))); ?></td>
+                                        <td>
+                                           <?php if($restorant->active == 1): ?>
+                                                <span class="badge badge-success"><?php echo e(__('Active')); ?></span>
+                                           <?php else: ?>
+                                                <span class="badge badge-warning"><?php echo e(__('Not active')); ?></span>
+                                           <?php endif; ?>
+                                        </td>
+                                        <td class="text-right">
+                                            <div class="dropdown">
+                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                    <a class="dropdown-item" href="<?php echo e(route('admin.restaurants.edit', $restorant)); ?>"><?php echo e(__('Edit')); ?></a>
+                                                    <a class="dropdown-item" href="<?php echo e(route('admin.restaurants.loginas', $restorant)); ?>"><?php echo e(__('Login as')); ?></a>
+                                                    <form action="<?php echo e(route('admin.restaurants.destroy', $restorant)); ?>" method="post">
+                                                        <?php echo csrf_field(); ?>
+                                                        <?php echo method_field('delete'); ?>
+                                                        <?php if($restorant->active == 0): ?>
+                                                            <a class="dropdown-item" href="<?php echo e(route('restaurant.activate', $restorant)); ?>"><?php echo e(__('Activate')); ?></a>
+                                                        <?php else: ?>
+                                                            <button type="button" class="dropdown-item" onclick="confirm('<?php echo e(__("Are you sure you want to deactivate this restaurant?")); ?>') ? this.parentElement.submit() : ''">
+                                                                <?php echo e(__('Deactivate')); ?>
+
+                                                            </button>
+                                                        <?php endif; ?>
+                                                    </form>
+                                                    <a class="dropdown-item warning red" onclick="return confirm('Are you sure you want to delete this Restaurant from Database? This will aslo delete all data related to it. This is irreversible step.')"  href="<?php echo e(route('admin.restaurant.remove',$restorant)); ?>"><?php echo e(__('Delete')); ?></a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer py-4">
+                        <nav class="d-flex justify-content-end" aria-label="...">
+                            <?php echo e($restorants->links()); ?>
+
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php echo $__env->make('layouts.footers.auth', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', ['title' => __('Restaurants')], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\xampp\htdocs\fastgo\resources\views/restorants/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/a029574df5d3e4fa5ef98d5dd9ada95962032ad1.php
@@ -0,0 +1,170 @@
+<div class="modal fade" id="productModal" z-index="9999" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-" role="document" id="modalDialogItem">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 id="modalTitle" class="modal-title" id="modal-title-new-item"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body p-0">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <div class="row">
+                            <div class="col-sm col-md col-lg text-center" id="modalImgPart">
+                                <img id="modalImg" src="" width="295px" height="200px">
+                            </div>
+                            <div class="col-sm col-md col-lg col-lg" id="modalItemDetailsPart">
+                                <input id="modalID" type="hidden"></input>
+                                <span id="modalPrice" class="new-price"></span>
+                                <p id="modalDescription"></p>
+                                <div id="variants-area">
+                                    <label class="form-control-label"><?php echo e(__('Select your options')); ?></label>
+                                    <div id="variants-area-inside">
+                                    </div>
+                                </div>
+                                <div id="exrtas-area">
+                                    <br />
+                                    <label class="form-control-label" for="quantity"><?php echo e(__('Extras')); ?></label>
+                                    <div id="exrtas-area-inside">
+                                    </div>
+                                </div>
+                               <?php if(  !(isset($canDoOrdering)&&!$canDoOrdering)   ): ?>
+                                <div class="quantity-area">
+                                    <div class="form-group">
+                                        <br />
+                                        <label class="form-control-label" for="quantity"><?php echo e(__('Quantity')); ?></label>
+                                        <!--<input type="number" name="quantity" id="quantity" class="form-control form-control-alternative" placeholder="1" value="1" required autofocus>-->
+                                            <input
+                                                    type="number"
+                                                    min="1"
+                                                    step="1"
+                                                    onkeypress="return event.charCode >= 48 && event.charCode <= 57"
+                                                    name="quantity" 
+                                                    id="quantity" 
+                                                    class="form-control form-control-alternative" 
+                                                    placeholder="1" 
+                                                    value="1" 
+                                                    required 
+                                                    autofocus
+                                            >
+                                    </div>
+                                    <div class="quantity-btn">
+                                        <div id="addToCart1">
+                                            <button class="btn btn-primary" v-on:click='addToCartAct'><?php echo e(__('Add To Cart')); ?></button>
+                                        </div>
+                                    </div>
+                                </div>
+                               <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="modal-import-restaurants" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
+    <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="modal-title-new-item"><?php echo e(__('Import restaurants from CSV')); ?></h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body p-0">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <div class="col-md-10 offset-md-1">
+                        <form role="form" method="post" action="<?php echo e(route('import.restaurants')); ?>" enctype="multipart/form-data">
+                            <?php echo csrf_field(); ?>
+                            <div class="form-group text-center<?php echo e($errors->has('item_image') ? ' has-danger' : ''); ?>">
+                                <label class="form-control-label" for="resto_excel">Import your file</label>
+                                <div class="text-center">
+                                    <input type="file" class="form-control form-control-file" name="resto_excel" accept=".csv, .ods, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" required>
+                                </div>
+                            </div>
+                            <input name="category_id" id="category_id" type="hidden" required>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary my-4"><?php echo e(__('Save')); ?></button>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php if(isset($restorant)): ?>
+<div class="modal fade" id="modal-restaurant-info" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
+    <div class="modal-dialog modal- modal-dialog-centered modal-lg" role="document" >
+        <div class="modal-content">
+            <div class="modal-body p-0">
+                <div class="card">
+                    <div class="card-header bg-white text-center">
+                        <img class="rounded img-center" src="<?php echo e($restorant->icon); ?>" width="90px" height="90px"></img>
+                        <h4 class="heading mt-4"><?php echo e($restorant->name); ?> &nbsp;<?php if(count($restorant->ratings)): ?><span><i class="fa fa-star" style="color: #dc3545"></i> <strong><?php echo e(number_format($restorant->averageRating, 1, '.', ',')); ?> <span class="small">/ 5 (<?php echo e(count($restorant->ratings)); ?>)</strong></span></span><?php endif; ?></h4>
+                        <p class="description"><?php echo e($restorant->description); ?></p>
+                        <?php if(!empty($openingTime) && !empty($closingTime)): ?>
+                            <p class="description"><?php echo e(__('Open')); ?>: <?php echo e($openingTime); ?> - <?php echo e($closingTime); ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <div class="card-body">
+                        <div class="nav-wrapper">
+                            <ul class="nav nav-pills nav-fill flex-column flex-md-row" id="tabs-icons-text" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link mb-sm-3 mb-md-0 active" id="tabs-icons-text-1-tab" data-toggle="tab" href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1" aria-selected="true"><?php echo e(__('About')); ?></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-icons-text-2-tab" data-toggle="tab" href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2" aria-selected="false"><?php echo e(__('Reviews')); ?></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="tabs-icons-text-1" role="tabpanel" aria-labelledby="tabs-icons-text-1-tab">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <h6 class="heading-small"><?php echo e(__('Phone')); ?></h6>
+                                        <p class="heading-small text-muted"><?php echo e($restorant->phone); ?></p>
+                                        <br/>
+                                        <h6 class="heading-small"><?php echo e(__('Address')); ?></h6>
+                                        <p class="heading-small text-muted"><?php echo e($restorant->address); ?></p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <div id="map3" class="form-control form-control-alternative"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="tabs-icons-text-2" role="tabpanel" aria-labelledby="tabs-icons-text-2-tab">
+                                <?php if(count($restorant->ratings) != 0): ?>
+                                    <br/>
+                                    <h5><?php echo e(count($restorant->ratings)); ?> <?php echo e(count($restorant->ratings) == 1 ? __('Review') : __('Reviews')); ?></h5>
+                                    <hr />
+                                    
+                                    <?php $__currentLoopData = $restorant->ratings; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rating): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <div class="strip">
+                                            <span class="res_title"><b><?php echo e($rating->user->name); ?></b></span><span class="float-right"><i class="fa fa-star" style="color: #dc3545"></i> <strong><?php echo e(number_format($rating->rating, 1, '.', ',')); ?> <span class="small">/ 5</strong></span></span><br />
+                                            <span class="text-muted"> <?php echo e($rating->created_at->format(env('DATETIME_DISPLAY_FORMAT','d M Y'))); ?></span><br/>
+                                            <br/>
+                                            <span class="res_description text-muted"><?php echo e($rating->comment); ?></span><br />
+                                        </div>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                <?php else: ?>
+                                  <p><?php echo e(__('There are no reviews yet.')); ?><p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
+
+
+<?php /**PATH E:\xampp\htdocs\fastgo\resources\views/restorants/partials/modals.blade.php ENDPATH**/ ?>
